a3e145dcffb0f51bccf88c24b1d6a230d1c91e11
monish karunakaran <monish.karunakaran@quicksort.dev>
2021-04-22 18:07:13 +0530

    styles

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import {Input} from "antd"
+import React,{useState} from 'react'
+import {Button, Input} from "antd"
+import "../../styles/Login.css"
+import Logo from "../../assets/icons/logo.png"
+import {field,btn} from "./styles"
+
 
 export default function Login() {
+    const [LoginFields, setLoginFields] = useState({username:"",password:""})
+    
+    const HandleChange=(e)=>{
+        setLoginFields({...LoginFields,[e.target.name]:e.target.value})
+    }
+
     return (
-        <div>
-         <Input placeholder="Username"/>   
-         <Input placeholder="Password"/>   
+        <div className="LoginRoot">
+         <div className="LoginCard">
+
+         <img src={Logo} className="LoginLogo"/>
+         
+         <Input 
+         placeholder="Username" 
+         name="usename" 
+         onChange={HandleChange} 
+         style={field}
+         type="text"/>
+
+         <Input 
+         placeholder="Password" 
+         name="password" 
+         onChange={HandleChange} 
+         style={field}
+         type="password"/>   
+
+         <Button 
+         style={btn}
+         onClick={()=>console.log(LoginFields)}>Login</Button>
+         </div>
         </div>
     )
 }
