2bf91397f1bd6ababa7588e4f4f7d93b6759c3fa
monish karunakaran <ckmonish2000@gmail.com>
2021-04-23 19:54:13 +0530

    added modal

--- src/components/Staff/AddStaffModal.js
+++ src/components/Staff/AddStaffModal.js
@@ -1,15 +1,65 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Modal, Button } from "antd";
 import { AdminContext } from "../../App";
 import { Input } from "antd";
+import { field } from "./styles";
 export default function AddStaffModal({ open, setopen }) {
   let context = useContext(AdminContext);
-  console.log(context);
-  let handleClose = () => setopen(false);
+  const [ModalParams, setModalParams] = useState({
+    email: "",
+    name: "",
+    password: "",
+    phone: "",
+    role: "",
+  });
+
+  // handlers
+  let handleClose = () => {
+    setopen(false);
+    setModalParams({
+      email: "",
+      name: "",
+      password: "",
+      phone: "",
+      role: "",
+    });
+  };
+
+  const HandleChange = (e) => {
+    setModalParams({ ...ModalParams, [e.target.name]: e.target.value });
+  };
+
   return (
     <Modal visible={open} title="Add User" onCancel={handleClose}>
-      <Input />
-      <Input />
+      <Input
+        name="email"
+        placeholder="Email"
+        value={ModalParams.email}
+        onChange={HandleChange}
+        style={field}
+      />
+      <Input
+        name="name"
+        placeholder="Name"
+        onChange={HandleChange}
+        value={ModalParams.name}
+        style={field}
+      />
+      <Input
+        name="password"
+        placeholder="Password"
+        onChange={HandleChange}
+        type="password"
+        value={ModalParams.password}
+        style={field}
+      />
+      <Input
+        name="phone"
+        placeholder="Phone"
+        onChange={HandleChange}
+        style={field}
+        value={ModalParams.phone}
+      />
     </Modal>
   );
 }
---
+++ src/components/Staff/styles.js
@@ -0,0 +1,3 @@
+let field = { borderRadius: "5pt", padding: "8pt", marginBottom: "20pt" };
+
+export { field };
