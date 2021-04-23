a97482f413870b8882ad1b51c9754ae4b3353aa0
monish karunakaran <ckmonish2000@gmail.com>
2021-04-23 14:33:53 +0530

    removed cacheing and added proper superuserlogic

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,22 +1,42 @@
 import React from "react";
-import { getPermissions, getRoles } from "../../services/queries";
+import { getPermissions, getRoles, isSuperUser } from "../../services/queries";
 import { useQuery, useMutation } from "@apollo/client";
 import Nav from "../Nav/Nav";
+import { Spin, Space, message } from "antd";
+import { Redirect } from "react-router";
 
 export const AdminContext = React.createContext();
 export default function DashBoard() {
   let Roles = useQuery(getRoles);
+  let isSuperorNot = useQuery(isSuperUser, {
+    fetchPolicy: "no-cache",
+  });
   let Permissions = useQuery(getPermissions);
-  return (
-    <AdminContext.Provider
-      value={{
-        Roles,
-        Permissions,
-      }}
-    >
+  // isSuperorNot.data?.me?isSuperuser
+  if (typeof isSuperorNot.data === "undefined") {
+    return (
       <div>
-        <Nav />
+        <Spin size="large" />
       </div>
-    </AdminContext.Provider>
-  );
+    );
+  } else {
+    if (isSuperorNot.data?.me?.isSuperuser) {
+      return (
+        <AdminContext.Provider
+          value={{
+            Roles,
+            Permissions,
+          }}
+        >
+          <div>
+            <Nav />
+          </div>
+        </AdminContext.Provider>
+      );
+    } else {
+      localStorage.removeItem("token");
+      message.warning("Permission Denied");
+      return <Redirect path="/login" />;
+    }
+  }
 }
--- src/utils/ProtectedRoutes.js
+++ src/utils/ProtectedRoutes.js
@@ -1,15 +1,10 @@
 import { message } from "antd";
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { isSuperUser } from "../services/queries";
-import { useQuery } from "@apollo/client";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  let isSuperorNot = useQuery(isSuperUser);
   let token = localStorage.getItem("token");
-  isSuperorNot = isSuperorNot.data?.me?.isSuperuser;
-  console.log(isSuperorNot);
-  if (token && isSuperorNot) {
+  if (token) {
     return (
       <Route {...rest} render={(props) => <Component {...rest} {...props} />} />
     );
